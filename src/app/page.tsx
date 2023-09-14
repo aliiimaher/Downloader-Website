e9c341f6395ab3f95343b85a04e27545bda30d21
aliiiimaher <alimohammadmaher@gmail.com>
2023-09-14 12:15:10 +0330

    feat: fonts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>this is main page...</h1>
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+          <div style={{ fontFamily: "pacifico" }}>this is main page...</div>
+        </h1>
+      </main>
+    </>
   );
 }
