8d6393f1a0c1119c0f4616263ea4ec32e26427d7
aliiiimaher <alimohammadmaher@gmail.com>
2023-09-14 13:24:15 +0330

    feat: body color and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>
-          <div style={{ fontFamily: "pacifico" }}>this is main page...</div>
-        </h1>
-      </main>
+      <body>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1>
+            <div style={{ fontFamily: "pacifico" }}>this is main page...</div>
+          </h1>
+        </main>
+      </body>
     </>
   );
 }
