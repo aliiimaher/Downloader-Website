45e4614d51ee74a7bef9ef97dd235928ee6b68dc
aliiiimaher <alimohammadmaher@gmail.com>
2023-09-16 11:16:57 +0330

    feat: add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Button from "../components/Button";
 import HomeIcon from "../assets/icons/home.svg";
 import Input from "@/components/Input";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <body>
         <main className="flex min-h-screen flex-col items-center justify-between p-[20px]">
+          <Header />
           <h1>
             <div style={{ fontFamily: "pacifico" }}>this is main page...</div>
             <Button icon={HomeIcon} text="Home" isSelectedBorder="yes" />
