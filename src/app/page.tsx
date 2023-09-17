6a7f1da4ecb4eed0e3d434ea415816c2143359e3
aliiiimaher <alimohammadmaher@gmail.com>
2023-09-17 16:16:18 +0330

    feat: finish route structure and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,46 @@
-import Button from "../components/Button";
-import HomeIcon from "../assets/icons/home.svg";
-import Input from "@/components/Input";
+import Button from "@/components/Button";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
+import YoutubeSvg from "../assets/icons/youtube.svg";
+import HomeSvg from "../assets/icons/home.svg";
+import InstagramSvg from "../assets/icons/insta.svg";
 
 export default function Home() {
   return (
     <>
-      <body>
-        <main className="flex min-h-screen flex-col items-center justify-between p-[20px]">
-          <Header />
-          <h1>
-            <div style={{ fontFamily: "pacifico" }}>this is main page...</div>
-            <Button icon={HomeIcon} text="Home" isSelectedBorder="yes" />
-            <Input btnType="youtube" placeHolder="bashe" />
-          </h1>
-        </main>
-        <Footer />
-      </body>
+      <Header />
+      <div
+        className={
+          "flex w-full flex-col items-start justify-center " +
+          "p-[20px] md:p-[225px]"
+        }
+      >
+        <div className="flex flex-row justify-between w-full">
+          <Button icon={YoutubeSvg} text="YouTube" />
+          <Button icon={HomeSvg} text="Home" isSelectedBorder="yes" />
+          <Button icon={InstagramSvg} text="Instagram" />
+        </div>
+        <p className="my-[40px]">
+          Hey body, welcome...
+          <br />
+          <br />
+          This is Debb Downloader, you can easily download
+          from the YouTube and the Instagram. All you need is a copy & past :)
+          <br />
+          <br />
+          You can select whether you want to download from the YouTube or
+          Instagram by clicking the related button in the header.
+          <br />
+          <br />
+          Other guidance are available in each pages. 
+          <br />
+          <br />
+          Finally, if you have any comments or
+          suggestions, please share them with me.
+        </p>
+      </div>
+      <Footer />
     </>
   );
 }
