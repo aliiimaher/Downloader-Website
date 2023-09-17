6a6b453721d990740aaf5bd548eae803cb141c08
aliiiimaher <alimohammadmaher@gmail.com>
2023-09-17 22:32:05 +0330

    style: cleancode and some css fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,32 +6,26 @@ export default function Header() {
   return (
     <>
       <div className="w-full flex flex-col place-items-center">
-        <div className="flex flex-row justify-center align-middle place-items-center h-[56px] w-[275px]">
-          <div
-            style={{
-              fontFamily: "pacifico",
-              fontSize: "32px",
-              marginRight: "4px",
-            }}
-          >
-            Debb
-          </div>
+        <div
+          className={
+            "flex flex-row justify-center align-middle" +
+            "place-items-center h-[56px] w-[275px]"
+          }
+        >
+          <div className="text-[32px] mr-[4px] font-['pacifico']">Debb</div>
           <Image
             src={WebsiteIconPng}
             alt="website-icon"
             className="w-[32px] h-[32px]"
           />
-          <p
-            style={{
-              fontFamily: "pacifico",
-              fontSize: "32px",
-              marginLeft: "4px",
-            }}
-          >
-            Downloader
-          </p>
+          <p className="text-[32px] ml-[4px] font-['pacifico']">Downloader</p>
         </div>
-        <div className="w-full border border-[#27285C] mt-[20px] mb-[16px] md:mb-[24px]"></div>
+        <div
+          className={
+            "w-full border border-[#27285C] mt-[20px] mb-[16px] " +
+            "md:mb-[24px] max-w-[990px]"
+          }
+        ></div>
       </div>
     </>
   );
